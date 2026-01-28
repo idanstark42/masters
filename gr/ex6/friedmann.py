21cecd2895b689e264d5e4beafba503a820b40b3
Idan Stark <idanstark42@gmail.com>
2026-01-28 12:17:43 +0200

    gr ex6

--- gr/ex6/friedmann.py
+++ gr/ex6/friedmann.py
@@ -1,77 +1,103 @@
-from sympy import Function, simplify
-from sympy import symbols
-from sympy.diffgeom import metric_to_Riemann_components, metric_to_Ricci_components, TensorProduct, Manifold, Patch, CoordSystem, metric_to_Riemann_components, TensorProduct
-from sympy.functions import sin
-from sympy.matrices import diag
+from einsteinpy.symbolic import EinsteinTensor, MetricTensor
+from sympy import symbols, sin, Function, solve
+from sympy.printing.latex import LatexPrinter
 
-c = symbols('c')  # Speed of light
-pi = symbols('pi')  # Pi constant
+# Constants
+c, NEWTON_G, pi = symbols('c G \\pi')
 
-def flatten(lst):
-  return [item for sublist in lst for item in sublist]
+# a printer that uses \dot and \ddot for time derivatives
+class CustomLatexPrinter(LatexPrinter):
+    def _print_Derivative(self, expr):
+        if len(expr.args) == 2:
+            func, der = expr.args
+            var, order = der
+            if var.is_Symbol and var.name == 't':
+                if order == 1:
+                    return r'\dot{' + func.name + '}(t)'
+                elif order == 2:
+                    return r'\ddot{' + func.name + '}(t)'
+        return super()._print_Derivative(expr)
 
-def friedmann_metric_components(a, k):
-  t, r, theta, phi = symbols('t r theta phi')
-  return diag(
-      -1,
-      a**2/(1 - k*r**2),
-      a**2*r**2,
-      a**2*r**2*sin(theta)**2
-  )
+# Latex conversion function
+def latex(expr):
+    return CustomLatexPrinter().doprint(expr)
 
-def friedmann_metric (a, k):
-  # Define the manifold and coordinate system
-  M = Manifold('M', 4)
-  P = Patch('P', M)
-  t, r, theta, phi = symbols('t r theta phi')
-  coords = CoordSystem('coords', P, [t, r, theta, phi])
-  t, r, theta, phi = coords.coord_functions()
-  oneforms = coords.base_oneforms()
+# Helper function to create diagonal matrices
+def diag(*args):
+    size = len(args)
+    return [[args[i] if i == j else 0 for j in range(size)] for i in range(size)]
 
-  # Metric components
-  components = friedmann_metric_components(a, k)
+# Define the Friedmann metric class
+class FriedmannMetric(MetricTensor):
+    def __init__(self, coords, a, k, signature_sign=1):
+        (t, r, theta, phi) = coords
+        g = diag(c**2,
+            -a**2/(1 - k*r**2),
+            -a**2 * r**2,
+            -a**2 * r**2 * sin(theta)**2)
+        super().__init__(g, coords, name="Friedmann Metric")
 
-  # Construct the metric tensor
-  return sum([sum([components[i, j] * TensorProduct(oneforms[i], oneforms[j]) for j in range(4)]) for i in range(4)])
+def main():
+    t, r, theta, phi, k = symbols('t r theta phi k')
+    a = Function('a')(t)
+    
+    # The metric
+    metric = FriedmannMetric((t, r, theta, phi), a, k)
+    g = metric.tensor()
+    print("g_{\\mu \\nu} = " + latex(g))
+    print()
+    
+    # The Einstein tensor
+    G = EinsteinTensor.from_metric(metric)
+    G_comps = G.tensor()
+    print("G_{\\mu \\nu} = " + latex(G_comps))
+    print()
+    
+    # The energy-momentum tensor
+    p, rho = symbols('p \\rho')
+    T = diag(rho * c**4, -p * g[1][1], -p * g[2][2], -p * g[3][3])
+    print("T_{\\mu \\nu} = " + latex(T))
+    print()
 
-def friedmann_riemann (a, k):
-  g = friedmann_metric(a, k)
-  Riemann = metric_to_Riemann_components(g)
-  return Riemann
+    # The Einstein equations
+    equations = [[G_comps[i][j] - 8 * pi * NEWTON_G / c**4 * T[i][j] for j in range(4)] for i in range(4)]
+    none_vanishing_equations = [eq for row in equations for eq in row if eq != 0]
+    for eq in none_vanishing_equations:
+        print(latex(eq) + " = 0")
+    print()
 
-def friedmann_ricci (a, k):
-  g = friedmann_metric(a, k)
-  Ricci = metric_to_Ricci_components(g)
-  return Ricci
+    # simplify and print the two independent equations
+    eq1 = none_vanishing_equations[0].simplify(rational=True)
+    eq2 = none_vanishing_equations[1].simplify(rational=True)
+    for eq in [eq1, eq2]:
+        print(latex(eq) + " = 0")
+    print()
 
-def friedmann_scalar_curvature (a, k):
-  Ricci = friedmann_ricci(a, k)
-  g = friedmann_metric_components(a, k)
-  return sum([g[i, j] * Ricci[i, j] for i in range(4) for j in range(4)])
+    # replacing \dot{a} and \ddot{a} with H and \dot{H}
+    H = Function('H')(t)
+    a_dot = H * a
+    a_ddot = a * H**2 + a * H.diff(t)
+    eq1_H = eq1.subs({a.diff(t): a_dot, a.diff(t, 2): a_ddot}).simplify()
+    eq2_H = eq2.subs({a.diff(t): a_dot, a.diff(t, 2): a_ddot}).simplify()
+    for eq in [eq1_H, eq2_H]:
+        print(latex(eq) + " = 0")
+    print()
 
-def friendmann_einstein_tensor (a, k):
-  Ricci = friedmann_ricci(a, k)
-  R = friedmann_scalar_curvature(a, k)
-  g = friedmann_metric_components(a, k)
+    # isolate H^2 and \dot{H}
+    sol = solve([eq1_H, eq2_H], (H**2, H.diff(t)))
+    H_squared_eq = sol[H**2].simplify(rational=True)
+    H_dot_eq = sol[H.diff(t)].simplify(rational=True)
+    print("H^2 = " + latex(H_squared_eq))
+    print("\\\\")
+    print("dot{H} = " + latex(H_dot_eq))
+    print()
 
-  return [[Ricci[i, j] - 0.5 * R * g[i, j] for j in range(4)] for i in range(4)]
+    # Now use \dot{H} = \ddot{a}/a - H^2 to express \ddot{a}/a
+    acceleration_eq = (H_dot_eq + H_squared_eq).factor()
 
-def friedmann_energy_momentum_tensor (a, k, rho, p, u):
-  g = friedmann_metric_components(a, k)
-  return [[(rho + p / c ** 2) * u[i] * u[j] - p * g[i, j] for j in range(4)] for i in range(4)]
-
-def friedmann_equations (a, k, rho, p):
-  G = friendmann_einstein_tensor(a, k)
-  T = friedmann_energy_momentum_tensor(a, k, rho, p, [1, 0, 0, 0])
-
-  return flatten([[G[i][j] - (8 * pi / c ** 4) * T[i][j] for j in range(4)] for i in range(4)])
+    print("H^2 = " + latex(H_squared_eq))
+    print("\\\\")
+    print("\\frac{\\ddot{a}}{a} = " + latex(acceleration_eq))
 
 if __name__ == "__main__":
-  t, k, rho, p = symbols('t k rho p')
-  a = Function('a')(t)
-
-  equations = friedmann_equations(a, k, rho, p)
-  simplified_equations = [simplify(eq) for eq in equations]
-
-  for i, eq in enumerate(simplified_equations):
-    print(f"Equation {i+1}: {eq}")
+    main()
